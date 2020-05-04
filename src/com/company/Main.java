721dbb8381f1385d644d86adc1ff235172c1b091
Nastya <realovka@mail.ru>
2020-05-04 12:52:20 +0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int sum=0;
+
+        for (int i=1; i<=n; i++)
+            sum+=i*i;
+        System.out.println("Сумма квадратов равна "+sum);
+    }
+}
